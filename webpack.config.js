f64094a267b4ff80519fccbf013499f68b161730
Leidson Cruz <leidson.cruz@corp.globo.com>
2018-12-05 17:20:02 -0200

    adicionando webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/js/Stories.js',
     output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '../dist')
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, './dist')
     }
 };
