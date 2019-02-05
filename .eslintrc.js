3c2a7e4fb9fe835de8e0a98aacab1216598f1edc
Vitor Rocha <vitor.rocha@corp.globo.com>
2019-02-05 19:45:28 -0200

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,30 @@
 module.exports = {
   "env": {
     "browser": true,
-  }
+    "es6": true,
+    "jest/globals": true
+  },
+  "plugins": ["jest"],
+  "parserOptions": {
+    "sourceType": "module",
+  },
+  "extends": ["eslint:recommended", "plugin:jest/recommended"],
+  "rules": {
+    "quotes": [ 1, "single" ],
+    "keyword-spacing": 1,
+    "space-before-function-paren": [1, "never"],
+    "sort-imports": [1, {}],
+    "indent": [1, 2],
+    "curly": [1, "all"],
+    "brace-style": [1, "1tbs", { "allowSingleLine": false }],
+    "space-before-blocks": [1, "always"],
+    "arrow-parens": [1, "as-needed"],
+    "space-infix-ops": 1,
+    "max-statements-per-line": [2, { "max": 1 }],
+    "no-console": 2,
+    "semi": 2,
+    "no-extra-semi": 2,
+    "no-unused-vars": [2, { "args": "all" }]
+  },
+  "globals": {}
 };
